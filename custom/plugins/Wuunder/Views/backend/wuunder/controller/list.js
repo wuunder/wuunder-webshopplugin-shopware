624f082f9d26fdad8c27ab6f357f40e9e28bf799
Timo <tj.timo.j@gmail.com>
2017-12-07 09:40:55 +0100

    use baseurl is smarty template

--- custom/plugins/Wuunder/Views/backend/wuunder/controller/list.js
+++ custom/plugins/Wuunder/Views/backend/wuunder/controller/list.js
@@ -19,9 +19,10 @@ Ext.define('Shopware.apps.Wuunder.controller.List', {
     },
 
     onShipOrder: function (record) {
+        var baseUrl = ("{config name='base_url' namespace='Wuunder'}");
         Ext.Ajax.request({
             method: 'POST',
-            url: '/shopware/backend/wuunder_shipment/redirect',
+            url: baseUrl + '/backend/wuunder_shipment/redirect',
             params: { order_id: record.get('id') },
             success: function (response, opts) {
                 var data = Ext.decode(response.responseText);
