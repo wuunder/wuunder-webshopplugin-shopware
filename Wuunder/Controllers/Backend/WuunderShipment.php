9effe69fbb0888544e61dd8ef0b351e3fce1c5fc
Timo Janssen <tj.timo.j@gmail.com>
2020-02-25 12:58:49 +0100

    Added support for order info from order booked via  shopware API

--- Wuunder/Controllers/Backend/WuunderShipment.php
+++ Wuunder/Controllers/Backend/WuunderShipment.php
@@ -88,7 +88,13 @@ class Shopware_Controllers_Backend_WuunderShipment extends Enlight_Controller_Ac
             try {
                 $description .= '- ' . $orderDetail->getQuantity() . 'x ' . $orderDetail->getArticleName() . "\r\n";
                 $value += round($orderDetail->getPrice(), 2) * 100;
-                $weight += round($orderDetail->getArticleDetail()->getWeight(), 2) * 1000;
+                if (is_null($orderDetail->getArticleDetail()) || empty($orderDetail->getArticleDetail())) {
+                    $article_repo = Shopware()->Models()->getRepository('Shopware\Models\Article\Article');
+                    $article = $article_repo->find($orderDetail->getArticleId());
+                    $weight += $orderDetail->getQuantity() * round($article->getMainDetail()->getWeight(), 2) * 1000;
+                } else {
+                    $weight += $orderDetail->getQuantity() * round($orderDetail->getArticleDetail()->getWeight(), 2) * 1000;
+                }
             } catch (Exception $e) {
                 continue;
             }
